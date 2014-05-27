e4208c6d29d51fbc063977cfe7d4133a6a42c2f4
tangyi <ngn998@gmail.com>
2014-05-28 02:17:08 +0800

    first commit

---
+++ block.js
@@ -0,0 +1,24 @@
+var black_list = new Array();
+function recieveBlackList(msgEvent) {
+   if (msgEvent.name === "sendBlackList") {
+       black_list = msgEvent.message;
+       console.log("recieve blocklist");
+       console.log(msgEvent.message);
+       var threads;
+       threads = document.getElementsByClassName('t5');
+       for(var i = 0; i < threads.length; i++) {
+           var name = threads[i].getElementsByClassName('g-has-hovercard2')[0].childNodes[0].nodeValue;
+           if (black_list.indexOf(name) != -1) {
+               threads[i].setAttribute("style","display:none;");
+           }
+       }
+       
+   }
+}
+safari.self.addEventListener("message", recieveBlackList, false);
+
+window.addEventListener('load',
+                        function(){
+                            safari.self.tab.dispatchMessage("requireBlackList");
+                        },
+                        true);
